da923b3cbbc02252ecd38760f9d08b250b62fb2e
Nikita Gritsunov <pilotbro@yandex.ru>
2021-04-12 19:23:15 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,14 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car astonMartin = new Car();
+        astonMartin.setYear(1980);
+        astonMartin.setColor("Grey");
+        astonMartin.setModel("DB9");
+
+        System.out.println("Цвет вашего автомобиля: " + astonMartin.getColor());
+        System.out.println("Год создания вашего автомобиля: " + astonMartin.getYear());
+        System.out.println("Модель вашего автомобиля: " + astonMartin.getModel());
 
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,10 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+    Motorbike ducati = new Motorbike(2021, "red", "Multistrada");
 
+        System.out.println("Год выпуска вашего мотоцикла: " + ducati.getYear());
+        System.out.println("Цвет вашего мотоцикла: " + ducati.getColor());
+        System.out.println("Модель вашего мотоцикла: " + ducati.getModel());
     }
 }
