a04ef69d9e9c4ee7967b5c92bf68a3e6c817416a
Nikita Gritsunov <pilotbro@yandex.ru>
2021-04-14 08:43:01 +0300

    comlete day7 tasks

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,9 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
+        Airplane plane1 = new Airplane("cessna", 1980, 16, 2000);
+        Airplane plane2 = new Airplane("piper", 1982, 11, 1900);
+        Airplane.compareAirplanes(plane1, plane2);
 
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,24 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random rnd = new Random();
 
+        Player red1 = new Player(rnd.nextInt(11) + 90);
+        Player red2 = new Player(rnd.nextInt(11) + 90);
+        Player red3 = new Player(rnd.nextInt(11) + 90);
+        Player blue1 = new Player(rnd.nextInt(11) + 90);
+        Player blue2 = new Player(rnd.nextInt(11) + 90);
+        Player blue3 = new Player(rnd.nextInt(11) + 90);
+//        Player blue4 = new Player(rnd.nextInt(11) + 90);
+        Player.info();
+
+        for (int i = 0; i < 100; i++) {
+            red1.run();
+        }
+        System.out.println(red1.getStamina());
+        Player.info();
     }
 }
