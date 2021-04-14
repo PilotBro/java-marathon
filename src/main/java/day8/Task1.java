5c62f216713627a49212e8a90aab69d8954bb9bd
Nikita Gritsunov <pilotbro@yandex.ru>
2021-04-14 13:22:22 +0300

    comlete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,16 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        String str = "";
+//        for (int i = 1; i < 20001; i++) {
+//            str = str + i + " ";
+//        }
+//        System.out.println(str);
 
+        for (int i = 1; i < 20001; i++) {
+            sb.append(" " + i);
+        }
+        System.out.println(sb.toString());
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane plane = new Airplane("cessna", 1890, 1234, 3000);
+        System.out.println(plane);
 
     }
 }
