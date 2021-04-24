dde0d5d80a7dc4aaecb206910409fbf7b2eb0159
Nikita Gritsunov <pilotbro@yandex.ru>
2021-04-24 08:44:09 +0300

    complete day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,34 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("test");
 
+        printResult(file);
+
+    }
+
+    public static void printResult(File file) {
+        double sum = 0;
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String[] str = scanner.nextLine().split(" ");
+                for (int i = 0; i < str.length; i++) {
+                    sum += Double.parseDouble(str[i]);
+                }
+                sum /= str.length;
+                System.out.printf(Locale.FRANCE,  sum + " --> %.3f", sum);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,65 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
+        Random rnd = new Random();
+
+        try {
+            PrintWriter pw1 = new PrintWriter(file1);
+
+            for (int i = 0; i < 1000; i++) {
+                pw1.print(rnd.nextInt(101) + " ");
+            }
+            pw1.close();
+
+            PrintWriter pw2 = new PrintWriter(file2);
+            List<Double> doubleList = new ArrayList<>();
+            Scanner sc = new Scanner(file1);
+
+            while (sc.hasNext()) {
+                doubleList.add(Double.parseDouble(sc.next()));
+            }
+
+            for (int i = 0; i < 1000; i = i + 20) {
+                double sum = 0;
+                for (int j = i; j < i + 20; j++) {
+                    sum += doubleList.get(j);
+                }
+                pw2.print(sum / 20 + " ");
+                sum = 0;
+            }
+            pw2.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        printResult(file2);
+    }
+
+    public static void printResult(File file) {
+        try {
+            Scanner sc = new Scanner(file);
+            double sum = 0;
+            while (sc.hasNext()) {
+                sum += sc.nextDouble();
+            }
+            System.out.println((int)sum);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
+
 }
