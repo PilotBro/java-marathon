1428610543e861657ecb451a581ecbfc3a6c7241
Nikita Gritsunov <pilotbro@yandex.ru>
2021-04-24 22:05:13 +0300

    complete day17 tasks

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,13 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        ChessPiece[] str = new ChessPiece[8];
+        for (int i = 0; i < 4; i++) {
+            str[i] = ChessPiece.PAWN_WHITE;
+            str[i + 4] = ChessPiece.ROOK_BLACK;
+        }
+        for (ChessPiece s : str) {
+            System.out.print(s.name + " ");
+        }
     }
 }
